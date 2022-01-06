c53c2859673734d38351f9913b2918e6f4095f63
Shien <f0928652413@gmail.com>
2022-01-06 19:22:31 +0800

    Initial Commit

---
+++ ifElse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ifElse
+//
+//  Created by 方仕賢 on 2022/1/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
